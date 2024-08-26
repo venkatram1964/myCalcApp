7062e2ffb542f9a23443f34280fa8952fb372573
venkatram1964 <venkat.veerareddy@gmail.com>
2024-08-26 10:18:02 +0530

    initial commit

---
+++ calculator/src/main/java/com/venkat/Calculator.java
@@ -0,0 +1,5 @@
+package com.venkat;
+
+public class Calculator {
+
+}
---
+++ calculator/src/test/java/com/venkat/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.venkat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
